628d785499b819a1933453163d25827a402d070e
Cole Rossman <colerossman99@gmail.com>
2022-02-08 15:00:04 -0800

    data to data.js added

---
+++ data.js
@@ -0,0 +1,45 @@
+export const watches = [
+    {
+        id: '1',
+        name: 'Nautilus',
+        manufacturer: 'Patek Philippe',
+        material: 'Stainless steel',
+        price: '$35,000', 
+    },
+    {
+        id: '2',
+        name: 'Daytona',
+        manufacturer: 'Rolex',
+        material: 'Yellow Gold',
+        price: '$45,000', 
+    },
+    {
+        id: '3',
+        name: 'RM 055 Bubba Watson',
+        manufacturer: 'Richard Mille',
+        material: 'Ceramic',
+        price: '$400,000', 
+    },
+    {
+        id: '4',
+        name: 'Santos',
+        manufacturer: 'Cartier',
+        material: 'Stainless Steel',
+        price: '$6,300', 
+    },
+    {
+        id: '5',
+        name: 'HI-BEAT 36000',
+        manufacturer: 'Grand Seiko',
+        material: 'Stainless Steel',
+        price: '$6,400', 
+    },
+    {
+        id: '6',
+        name: 'Royal Oak Skeleton',
+        manufacturer: 'Audemars Piguet',
+        material: 'Stainless Steel',
+        price: '$200,000', 
+    },
+ 
+];
