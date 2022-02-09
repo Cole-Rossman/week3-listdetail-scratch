fc6a7eb57766f169c7f4ebb4041481fdefd008e6
Cole Rossman <colerossman99@gmail.com>
2022-02-08 18:33:09 -0800

    class watch-image added to test to reflect added class

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -3,7 +3,7 @@ import { renderListItem, renderWatch } from '../render-utils.js';
 const test = QUnit.test;
 
 test('renderListItem should render a div with watch details', (expect) => {
-    const expected = `<a href="./watches/?id=5"><div class="watch"><img src="/assets/gs36000.png"><span class="name">HI-BEAT 36000</span><span class="manufacturer">Grand Seiko</span></div></a>`;
+    const expected = `<a href="./watches/?id=5"><div class="watch"><img src="/assets/gs36000.png" class="watch-image"><span class="name">HI-BEAT 36000</span><span class="manufacturer">Grand Seiko</span></div></a>`;
 
     const grandSeiko = {
         id: '5',
@@ -19,7 +19,7 @@ test('renderListItem should render a div with watch details', (expect) => {
 });
 
 test('renderWatch should render a div with extended watch details', (expect) => {
-    const expected = `<div class="watch"><img src="/assets/gs36000.png"><p class="name">HI-BEAT 36000</p><p class="manufacturer">Grand Seiko</p><p class="material">Stainless Steel</p><p class="price">$6,400</p></div>`;
+    const expected = `<div class="watch"><img src="/assets/gs36000.png" class="watch-image"><p class="name">HI-BEAT 36000</p><p class="manufacturer">Grand Seiko</p><p class="material">Stainless Steel</p><p class="price">$6,400</p></div>`;
 
     const grandSeiko = {
         id: '5',
